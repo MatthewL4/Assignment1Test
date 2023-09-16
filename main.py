fa149defc28f12674fe52fe60f745e7340dec0e9
MatthewL4 <125594147+MatthewL4@users.noreply.github.com>
2023-09-15 20:09:01 -0400

    feat: add check_resources function

--- main.py
+++ main.py
@@ -45,6 +45,10 @@ class SandwichMachine:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
+        for ingredient, quantity in ingredients.items():
+            if self.machine_resources.get(ingredient) < quantity:
+                return False
+            return True
 
     def process_coins(self):
         """Returns the total calculated from coins inserted.
